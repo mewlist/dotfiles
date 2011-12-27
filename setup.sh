ba2f199de0eae1ddbc7b6042ece7c242cff58ffe
mewlist(Hidenori Doi) <mewlist@mewlist.com>
2011-12-27 13:09:05 +0900

    Add after/plugin

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
+ROOTDIR=`pwd`
+
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cd ./vim_plugins/snipmate-snippets
 rake deploy_local
+cd $ROOTDIR
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
   curl -so ~/.vim/autoload/pathogen.vim \
   https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+
+cp -r ./.vim/after ~/.vim/
+
+vim -u .vimrc +BundleInstall +q +q
